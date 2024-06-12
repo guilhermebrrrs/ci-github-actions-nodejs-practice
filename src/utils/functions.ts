0196f6eecae0992290a9f7d9f3de61f40e95d205
Guilherme Barra Orsi <guilhermebrrrs@hotmail.com>
2024-06-12 11:48:40 -0300

    test: corrige sonar-project.properties

---
+++ src/utils/functions.ts
@@ -0,0 +1,15 @@
+export const somar = (a: number, b: number) => a + b;
+
+export const subtrair = (a: number, b: number) => a - b;
+
+export const multiplicar = (a: number, b: number) => a * b;
+
+export const dividir = (a: number, b: number) => a / b;
+
+export const dividir2 = (ab: number, bb: number) => ab / bb;
+
+export const dividir3 = (ac: number, bc: number) => ac / bc;
+
+export const dividir4 = (ad: number, bd: number) => ad / bd;
+
+export const dividir5 = (ae: number, be: number) => ae / be;
